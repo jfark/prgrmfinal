6de1965e33e5c77c15726e4d216b5a024561f2ff
jfark <jessamyn.farkas@gmail.com>
2019-05-09 23:52:57 -0400

    Interesting Progress

--- script.js
+++ script.js
@@ -5,87 +5,87 @@ Vue.component ('the-portfolio', {
 			<h3 class="nodeTitle">{{title}}</h3>
 			<p class="nodeCaption">{{caption}}</p>
 			<div class="nodeImageContainer"
-				<div class="nodeImage"><img v-bind:src="image" alt="Name"/></div>
-			</div>
-			<div class="nodeImageContainer2"
-				<div class="nodeImage2"><img v-bind:src="image2" alt="Name"/></div>
+				<div class="nodeImage"><img v-bind:src="image" alt="title"/></div>
 			</div>
 		</div>
 	`
 });
 
-var signedBooksCollection = new Vue ({
-	el: ".portfolioProjects",
+var portfolioProjects = new Vue ({
+	el: ".portfolioOverall",
 	data: {
+		titleInput: "",
+		captionInput: "",
+		imageInput: "",
 		portfolio: [
 			{
 				title: "Barnes Foundation Fall 2019 Course Catalog",
 				image: 'images/barnes.jpg',
-				image2: 'images/garf.png',
 				caption: "I am currently working as a freelance graphic designer, completing the graphic design of the Barnes Foundation",
 			},
 			{
 				title: "DuBois + Baldwin Reader / MCAD Spring 2019: Typography",
 				image: 'images/reader.png',
-				image2: 'images/garf.png',
 				caption: "I am currently working as a freelance graphic designer, completing the graphic design of the Barnes Foundation",
 			},
 			{
 				title: "Future Shuttle Album Art",
 				image: 'images/fs.jpg',
-				image2: 'images/garf.png',
 				caption: "I am currently working as a freelance graphic designer, completing the graphic design of the Barnes Foundation",
 			},
 			{
 				title: "Digital Animation",
 				image: 'images/plants.gif',
-				image2: 'images/garf.png',
 				caption: "I am currently working as a freelance graphic designer, completing the graphic design of the Barnes Foundation",
 			},
 			{
 				title: "Hand & Digitally-Drawn Typeface / MCAD Spring 2019: Typography",
 				image: 'images/typeface.png',
-				image2: 'images/garf.png',
 				caption: "I am currently working as a freelance graphic designer, completing the graphic design of the Barnes Foundation",
 			},
 			{
 				title: '"My Practice" / MCAD Fall 2018: Design in Context',
 				image: 'images/practice.png',
-				image2: 'images/garf.png',
 				caption: "I am currently working as a freelance graphic designer, completing the graphic design of the Barnes Foundation",
 			},
 			{
 				title: "Isabella Stuart Gardner Homepage Recreation / MCAD Fall 2018: Web Development",
 				image: 'images/isg.png',
-				image2: 'images/garf.png',
 				caption: "I am currently working as a freelance graphic designer, completing the graphic design of the Barnes Foundation",
 			},
 			{
 				title: "Oberlin College Press Website Exercise / MCAD Fall 2018: Web Development",
 				image: 'images/ocpress.png',
-				image2: 'images/garf.png',
 				caption: "I am currently working as a freelance graphic designer, completing the graphic design of the Barnes Foundation",
 			},
 			{
 				title: "Poster / MCAD Fall 2018: Design in Context",
 				image: 'images/poster1.png',
-				image2: 'images/garf.png',
 				caption: "I am currently working as a freelance graphic designer, completing the graphic design of the Barnes Foundation",
 			},
 			{
 				title: "Annotated Poster / MCAD Fall 2018: Design in Context",
 				image: 'images/poster2.png',
-				image2: 'images/garf.png',
 				caption: "I am currently working as a freelance graphic designer, completing the graphic design of the Barnes Foundation",
 			},
 			{
 				title: "Future Shuttle Website Exercise / MCAD Fall 2018: Web Development",
 				image: 'images/fssite.png',
-				image2: 'images/garf.png',
 				caption: "I am currently working as a freelance graphic designer, completing the graphic design of the Barnes Foundation",
 			},
 		]
 	},
+	methods: {
+			addProject: function(){
+				let project = {
+					title: this.titleInput,
+					image: this.imageInput,
+					caption: this.captionInput,
+				};
+				this.portfolio.push(project);
+				this.titleInput = this.imageInput = this.captionInput = "";
+			}
+		}
 });
 
 $(".portfolioNode").click(function () {
